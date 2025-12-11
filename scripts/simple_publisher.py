279d93040f51875c155b48075988f7d1a2e37b01
MattiaTinfena <mattia.tinfena@gmail.com>
2025-12-11 11:50:32 +0100

    Saving markers position

--- scripts/simple_publisher.py
+++ scripts/simple_publisher.py
@@ -12,6 +12,7 @@ class RobotControl(Node):
         super().__init__('robotControl')
         # self.timer = self.create_timer(0.1, self.)
         self.delta = 0.03
+        self.yaw = 0.0
         self.initial_yaw = None
         self.readyToStop = False
         self.markers_detected = dict()
@@ -38,30 +39,32 @@ class RobotControl(Node):
         
     def detections_callback(self, msg):
         if len(msg.markers) > 0:
-            #self.get_logger().info(f'marker id: {msg.markers[0].marker_id}, pose: {msg.markers[0].pose}')
             for marker in msg.markers:
-                self.markers_detected[marker.marker_id] = marker.pose
+                if marker.marker_id not in self.markers_detected or \
+                    abs(marker.pose.position.x) < abs(self.markers_detected[marker.marker_id][1].x):
+                   
+                    self.markers_detected[marker.marker_id] = (self.yaw,marker.pose.position)
 
     def odometry_callback(self, msg):
         
         sin_yaw = 2 * (msg.pose.pose.orientation.w * msg.pose.pose.orientation.z + msg.pose.pose.orientation.x * msg.pose.pose.orientation.y)
         cos_yaw = 1 - 2 * (msg.pose.pose.orientation.y * msg.pose.pose.orientation.y + msg.pose.pose.orientation.z * msg.pose.pose.orientation.z)
-        yaw = round(3.14 + math.atan2(sin_yaw, cos_yaw),2) 
+        self.yaw = round(3.14 + math.atan2(sin_yaw, cos_yaw),2) 
         #self.get_logger().info(f'yaw: {yaw}')
 
         if self.initial_yaw is not None:
-            near_starting_point = (self.initial_yaw  + self.delta) % 6.28 > yaw and (self.initial_yaw  - self.delta) % 6.28 < yaw
+            near_starting_point = (self.initial_yaw  + self.delta) % 6.28 > self.yaw and (self.initial_yaw  - self.delta) % 6.28 < self.yaw
             if not near_starting_point :
                 self.readyToStop = True
             if self.readyToStop and near_starting_point:
                 #self.get_logger().info(f'reached a complete turn with: {yaw}, initial yaw: {self.initial_yaw}, initial yaw -0.1: {self.initial_yaw - 0.1} stopping...')
                 self.readyToStop = False
                 self.velocity = Twist()
-                #self.get_logger().info(self.markers_detected)
+                self.get_logger().info(self.markers_detected)
                 self.get_logger().info(f'stopped')
         else:
             self.get_logger().info(f'started')
-            self.initial_yaw = yaw
+            self.initial_yaw = self.yaw
             self.spin_robot()
 
 
